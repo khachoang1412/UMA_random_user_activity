afb586901dca8ac49252f14f9d5798d7caab67fa
Khac-Hoang Ngo <59874624+khachoang1412@users.noreply.github.com>
2023-05-26 16:45:55 +0200

    remove unnecessary output variables

--- RCU_KaUnknown/golden_search_P1_MDFA.m
+++ RCU_KaUnknown/golden_search_P1_MDFA.m
@@ -23,8 +23,8 @@ k2 = 0;                       % iteration index
 x1 = START_INT+(1-tau)*(END_INT-START_INT);            
 x2 = START_INT+tau*(END_INT-START_INT);
 
-[fMD_x1,fFA_x1,fMD_x1_r,fFA_x1_r,p0_x1] = f(P,x1); 
-[fMD_x2,fFA_x2,fMD_x2_r,fFA_x2_r,p0_x2] = f(P,x2);
+[fMD_x1,fFA_x1] = f(P,x1); 
+[fMD_x2,fFA_x2] = f(P,x2);
 
 while ((abs(END_INT-START_INT)>TOL) && (k2<iter))
     if g(fMD_x1, fFA_x1) < g(fMD_x2, fFA_x2) %function smaller in x1 than in x2
@@ -33,18 +33,13 @@ while ((abs(END_INT-START_INT)>TOL) && (k2<iter))
         x1 = START_INT+(1-tau)*(END_INT-START_INT); %find new beginning
         fMD_x2 = fMD_x1;%already have value in x1
         fFA_x2 = fFA_x1;
-        fMD_x2_r = fMD_x1_r;
-        fFA_x2_r = fFA_x1_r;
-        p0_x2 = p0_x1;
         [fMD_x1,fFA_x1] = f(P,x1); %%compute new value for new beginning
     else
         START_INT=x1; %function smaller in x2 than in x1 so set new start indx to x1
         x1 = x2; %replace as new start index
         x2=START_INT+tau*(END_INT-START_INT); %compute new end index
         fMD_x1= fMD_x2;
-        fFA_x1 = fFA_x2;        fMD_x1_r = fMD_x2_r;
-        fFA_x1_r = fFA_x2_r;
-        p0_x1 = p0_x2;
+        fFA_x1 = fFA_x2;        
         [fMD_x2,fFA_x2] = f(P,x2);
     end
     k2=k2+1;
